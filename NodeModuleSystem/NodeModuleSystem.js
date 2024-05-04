35404c1cbaf9ee322736eee9b64c621599a27e41
Simon H-M <simonhm31@gmail.com>
2024-05-03 21:36:14 -0500

    awf

---
+++ NodeModuleSystem/NodeModuleSystem.js
@@ -0,0 +1,11 @@
+console.log(console);
+
+// Global Objects in JS
+    // setTimeout();
+    // clearTimeout();
+    // setInterval();
+    // clearInterval();
+
+
+globalThis.console.log("console is a property of the globalThis object");
+
