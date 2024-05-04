44567dc6f8eb2c7244e0dd094b23b0df7748e9d1
Simon H-M <simonhm31@gmail.com>
2024-05-04 14:41:08 -0500

    http module

--- NodeModuleSystem/HTTP-Module.js
+++ NodeModuleSystem/HTTP-Module.js
@@ -1,7 +1,23 @@
 const http = require("http");
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    if(req.url === "/" ){
+        res.write("Hello World");
+        res.end()
+    };
+
+    if(req.url === "/api/courses"){
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
+
+    if(req.url === "/yo/momma"){
+        res.write("Is cool");
+        res.end();
+    }
+});
 
 
-console.log(http);
-console.log(server);
+server.listen(3000);
+
+console.log("Listening on Port 3000...")
